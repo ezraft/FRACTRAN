a6a8a99880d927b990bee6f5723082aa2f43dcc9
ezraft <ezrafurtado-tiwari@ucsb.edu>
2025-05-07 10:05:49 -0700

    intitial commit including interpreter and basic programs

---
+++ README.md
@@ -0,0 +1,24 @@
+# FRACTRAN
+
+Online Interpreter: [https://tjwei.github.io/Fractran/](https://tjwei.github.io/Fractran/)
+
+## Swap
+Task: write a program that swaps register $2$ with register $3$ (i.e given $2^k$, return $3^k$).
+
+Code:
+
+```2/3```
+
+## Addition
+
+Task: write a program that takes two inputs in registers $2$ and $3$, and adds them, placing the result in register $5$ (i.e. given $2^k$ and $3^\ell$, return $5^{k+\ell}$).
+
+Code:
+```5/2 5/3```
+
+## Multiplication
+
+Task: write a program that takes two inputs in registers $2$ and $3$, and multiplies them, placing the result in register $5$ (i.e. given $2^k$ and $3^\ell$, return $5^{k+\ell}$).
+
+Code:
+```5/2 5/3```
---
+++ interpreter.py
@@ -0,0 +1,63 @@
+import sys
+print(sys.maxsize)
+# utilities
+small_prime_list = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59]
+
+# take number and return array of primes and multiplicities in factorization
+def get_info(num):
+    prime_factors = {}
+    num_copy = num
+    for prime in small_prime_list:
+        while num_copy % prime == 0:
+            num_copy //= prime
+            if prime not in prime_factors.keys():
+                prime_factors[prime] = 1
+            else:
+                prime_factors[prime] += 1
+    return prime_factors
+
+# input file
+if len(sys.argv) < 2:
+    print("Error: Provide input file.")
+    exit(1)
+
+#read program
+with open(sys.argv[1], 'r') as f:
+    asm = f.read().strip()
+
+#parse program
+unfiltered_lines = asm.split("\n")
+prog_input = int(unfiltered_lines[0])
+instruction_lines = unfiltered_lines[1:]
+inst = [[[int(x) for x in ins.split(" ")[0].split('/')] +[int(ins.split(" ")[1])] for ins in line.split(",")] for line in instruction_lines]
+
+# for debugging
+# print(prog_input)
+# print(inst)
+
+#run program
+pc = 0
+prog_val = prog_input
+
+while True:
+    # state for debug
+    print("pc", pc)
+    print(prog_val)
+    print(get_info(prog_val))
+    # go = input("-> Step? ")
+    # if go != 'y':
+    #     break
+
+    ex_inst = False
+    for frac in inst[pc]:
+        if prog_val % frac[1] == 0:
+            prog_val = prog_val // frac[1]
+            prog_val = prog_val * frac[0]
+            pc = frac[2] - 1
+            ex_inst = True
+            break
+    if not ex_inst:
+        break
+
+print(prog_val)
+print(get_info(prog_val))
